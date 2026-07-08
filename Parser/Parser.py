778e6f567ba2ffb4054f0b3b9e8ed7dfddf3cd29
zenksoo <contactme.abdelilah@gmail.com>
2026-07-08 18:44:50 +0100

    updated mapParser

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -4,32 +4,33 @@ from typing import List
 class WCfgParser:
     pass
 
+import re
+
+line = "nb_drones: 23"
+
+thematch = re.match(
+    r"", line)
 
 class MapParser:
     def __init__(self, map_file_path: str) -> None:
         self.map_file = open(map_file_path, 'r')
 
     def parse(self) -> None:
-        data = {
-            "nb_drones": 0,
-            "hubs": {},
-            "connections": []
-        }
+        # data = {
+        #     "nb_drones": 0,
+        #     "hubs": {},
+        #     "connections": []
+        # }
         content_lines = self.map_file.readlines()
         self.map_file.close()
 
         for line in content_lines:
-            line = line.strip()
-            if (line.startswith("#") or not len(line)):
+            if line.startswith("#") or not len(line.strip()):
                 continue
-            line = line.split(":")
-            if line[0] == "start_hub":
-                data["hubs"]["type"] = "start_hub"
-                data["hubs"]["name"] = 
+            line = line.split("#")[0].strip()
             print(line)
-            # if line.startswith("nb_drones"):
-            #     data["nb_drones"] = int(line.split(":")[1])
-
+            # if (line.startswith("#") or not len(line)):
+            #     continue
 
         print(data)
 
